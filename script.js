fb7dfc736bd3ba0e8caf267fcdd3b64484870c2d
Viktorija Jenca <lalukins23@gmail.com>
2021-02-13 13:41:25 +0200

    Lesson 36 Promise realization

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 let url = window.location.toString();
-const getName = (url) => {
+let preloader = document.getElementById('loading');
+  setTimeout(function() {
+  preloader.classList.add('hide');
+}, 5000);
+
+const getInfo = (url) => {
   let url2 = url.split('=');
   let name = url2[1];
   if (name == undefined) {
     name = 'Lalukins';
   }
-return name;
+ let url1 = 'https://api.github.com/users/'+ name;
+return url1;
 }
 
-fetch(`https://api.github.com/users/${getName(url)}`)
+let getURL = getInfo(url);
+
+const getName = new Promise((resolve,reject) => {
+  setTimeout(() => getURL ? resolve(getURL) : reject('Имя неопределено'), 2000);
+});
+
+let now = new Date();
+const getDate = new Promise ((resolve, reject) => {
+  setTimeout(() => now ? resolve(now) : reject('Дата неопределена'), 5000);
+});
+
+Promise.all([getDate, getName])
+  .then(([now,getURL]) => fetch(getURL))
   .then(res => res.json())
   .then(json => {
     console.log(json.avatar_url);
@@ -25,5 +43,9 @@ fetch(`https://api.github.com/users/${getName(url)}`)
     let img = document.createElement('img');
     img.src = json.avatar_url;
     document.body.append(img);
+    let date = document.createElement('p');
+    date.innerHTML = now;
+    document.body.append(date);
+    console.log(now);
 })
   .catch(err => alert('Информация о пользователе недоступна'));
